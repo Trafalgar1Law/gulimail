037343669c0d99d7d1ce5cde8fc1e69537082e92
Jesse <1250503713@qq.com>
2019-08-01 23:04:40 +0800

    pms自动生成和测试

---
+++ gulimall-pms/src/main/java/com/atguigu/gulimall/pms/dao/ProductAttrValueDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.pms.dao;
+
+import com.atguigu.gulimall.pms.entity.ProductAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * spu属性值
+ * 
+ * @author Jesse
+ * @email Jesse@atguigu.com
+ * @date 2019-08-01 21:54:13
+ */
+@Mapper
+public interface ProductAttrValueDao extends BaseMapper<ProductAttrValueEntity> {
+	
+}
---
+++ gulimall-pms/src/main/java/com/atguigu/gulimall/pms/dao/SkuInfoDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.pms.dao;
+
+import com.atguigu.gulimall.pms.entity.SkuInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku信息
+ * 
+ * @author Jesse
+ * @email Jesse@atguigu.com
+ * @date 2019-08-01 21:54:13
+ */
+@Mapper
+public interface SkuInfoDao extends BaseMapper<SkuInfoEntity> {
+	
+}
